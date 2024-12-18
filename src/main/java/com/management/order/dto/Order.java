9d6c503d58b5bc3a250d1cce6f466118e53abc2f
Renato Mello <renatomdemello@gmail.com>
2024-12-18 20:26:10 -0300

    updated Order Entity and added the postman collection.

--- src/main/java/com/management/order/dto/Order.java
+++ src/main/java/com/management/order/dto/Order.java
@@ -1,24 +1,21 @@
 package com.management.order.dto;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.CascadeType;
+import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
-    private List<Product> products;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> products = new ArrayList<>();
 
     private String externalId;
     private Double totalValue;
